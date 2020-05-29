76d983f0f26edc8f9d87a5d62dd0cff85509d80d
Arthur Neto <netofarthur@gmail.com>
2020-05-29 13:18:43 -0300

    Updated Models

--- laudario/masks/models.py
+++ laudario/masks/models.py
@@ -4,50 +4,51 @@ from django.db import models
 
 class Exam(models.Model):
     id = models.AutoField(primary_key=True) 
-    description_l = models.CharField(max_length=50)
-    description_s = models.CharField(max_length=25)
+    long_description = models.CharField(max_length=50)
+    short_description = models.CharField(max_length=25)
     initials = models.CharField(max_length=3)
     def __str__(self):
-        return self.description_l
+        return self.long_description
     
 class Specialty(models.Model):
     id = models.AutoField(primary_key=True)
-    description_l = models.CharField(max_length=50)
-    description_s = models.CharField(max_length=25)
+    long_description = models.CharField(max_length=50)
+    short_description = models.CharField(max_length=25)
     def __str__(self):
-        return self.description_l
-
+        return self.long_description
 
 class System(models.Model):
     id = models.AutoField(primary_key=True)
-    description_l = models.CharField(max_length=50)
-    description_s = models.CharField(max_length=25)
+    long_description = models.CharField(max_length=50)
+    short_description = models.CharField(max_length=25)
     specialty = models.ForeignKey(Specialty, on_delete=models.CASCADE)
     def __str__(self):
-        return self.description_l
+        return self.long_description
+
 
 
 
 class Organ(models.Model):
     id = models.AutoField(primary_key=True)
-    description_l = models.CharField(max_length=50)
-    description_s = models.CharField(max_length=25)
+    long_description = models.CharField(max_length=50)
+    short_description = models.CharField(max_length=25)
     system = models.ForeignKey(System, on_delete=models.CASCADE)
     def __str__(self):
-        return self.description_l
+        return self.long_description
+
 
 class Mask(models.Model):
     id = models.AutoField(primary_key=True)
     system = models.ForeignKey(System, on_delete=models.CASCADE)
     exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
-    contraste = False
-    nome_mascara = models.CharField(max_length=50)
-    titulo = models.CharField(max_length=50)
-    tecnica_header = models.CharField(max_length=50)
-    tecnica = models.CharField(max_length=500)
-    relatorio_header = models.CharField(max_length=500)  
+    contrast = False
+    mask_name = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    technique_header = models.CharField(max_length=50)
+    technique = models.CharField(max_length=500)
+    report_header = models.CharField(max_length=500)  
     def __str__(self):
-        return self.nome_mascara
+        return self.mask_name
 
         
  
