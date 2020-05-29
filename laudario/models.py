0b53ef55198ae5ed488c0b189b511571440639cb
Arthur Neto <netofarthur@gmail.com>
2020-05-29 02:53:09 -0300

    Commited everything that wasn't tracked

--- laudario/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Exame(models.Model):
-	description_l = models.CharField(max_length=50)
-	description_s = models.CharField(max_length=25)
-	sigla = models.CharField(max_length=3)
