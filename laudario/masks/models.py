c825e5f7832b13469674d6b62951942d87d8804c
Arthur Neto <netofarthur@gmail.com>
2020-05-29 12:46:30 -0300

    Fixed name of fields on tables on admin interface

--- laudario/masks/models.py
+++ laudario/masks/models.py
@@ -7,23 +7,34 @@ class Exam(models.Model):
     description_l = models.CharField(max_length=50)
     description_s = models.CharField(max_length=25)
     initials = models.CharField(max_length=3)
+    def __str__(self):
+        return self.description_l
     
 class Specialty(models.Model):
     id = models.AutoField(primary_key=True)
     description_l = models.CharField(max_length=50)
     description_s = models.CharField(max_length=25)
+    def __str__(self):
+        return self.description_l
+
 
 class System(models.Model):
     id = models.AutoField(primary_key=True)
     description_l = models.CharField(max_length=50)
     description_s = models.CharField(max_length=25)
     specialty = models.ForeignKey(Specialty, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.description_l
+
+
 
 class Organ(models.Model):
     id = models.AutoField(primary_key=True)
     description_l = models.CharField(max_length=50)
     description_s = models.CharField(max_length=25)
     system = models.ForeignKey(System, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.description_l
 
 class Mask(models.Model):
     id = models.AutoField(primary_key=True)
@@ -35,7 +46,9 @@ class Mask(models.Model):
     tecnica_header = models.CharField(max_length=50)
     tecnica = models.CharField(max_length=500)
     relatorio_header = models.CharField(max_length=500)  
-    
+    def __str__(self):
+        return self.nome_mascara
+
         
  
 
