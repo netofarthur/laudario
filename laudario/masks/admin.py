86972755d31a47465fafb24ac05721420556ca8b
Arthur Neto <netofarthur@gmail.com>
2020-05-29 13:55:21 -0300

    Setting up database

--- laudario/masks/admin.py
+++ laudario/masks/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Exam, Organ, System, Specialty
+from .models import Exam, Organ, System, Specialty, Mask
 
 admin.site.register (Exam)
 admin.site.register (Organ)
 admin.site.register (System)
 admin.site.register (Specialty)
-
+admin.site.register (Mask)
--- laudario/masks/models.py
+++ laudario/masks/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 
 class Exam(models.Model):
-    id = models.AutoField(primary_key=True) 
     long_description = models.CharField(max_length=50)
     short_description = models.CharField(max_length=25)
     initials = models.CharField(max_length=3)
@@ -11,14 +10,12 @@ class Exam(models.Model):
         return self.long_description
     
 class Specialty(models.Model):
-    id = models.AutoField(primary_key=True)
     long_description = models.CharField(max_length=50)
     short_description = models.CharField(max_length=25)
     def __str__(self):
         return self.long_description
 
 class System(models.Model):
-    id = models.AutoField(primary_key=True)
     long_description = models.CharField(max_length=50)
     short_description = models.CharField(max_length=25)
     specialty = models.ForeignKey(Specialty, on_delete=models.CASCADE)
@@ -27,18 +24,17 @@ class System(models.Model):
 
 
 
-
+    #Available Organs (it has to be updated when a new organ is added. 
 class Organ(models.Model):
-    id = models.AutoField(primary_key=True)
     long_description = models.CharField(max_length=50)
     short_description = models.CharField(max_length=25)
+    string_variable = models.CharField(max_length=25)
     system = models.ForeignKey(System, on_delete=models.CASCADE)
     def __str__(self):
         return self.long_description
 
-
+#Normal masks (templates)
 class Mask(models.Model):
-    id = models.AutoField(primary_key=True)
     system = models.ForeignKey(System, on_delete=models.CASCADE)
     exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
     contrast = False
@@ -46,20 +42,11 @@ class Mask(models.Model):
     title = models.CharField(max_length=50)
     technique_header = models.CharField(max_length=50)
     technique = models.CharField(max_length=500)
-    report_header = models.CharField(max_length=500)  
+    report_header = models.CharField(max_length=500)
+
+    
+
     def __str__(self):
         return self.mask_name
-
-        
- 
-
- 
-
-
-
-
-
-
-
 
 
