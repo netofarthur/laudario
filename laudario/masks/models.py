e627f7d6127384c1c92f9547653443e72bd9576c
Arthur Neto <netofarthur@gmail.com>
2020-06-01 23:07:54 -0300

    Alterado modelo do banco de dados, com relação one to many adequada

--- laudario/masks/models.py
+++ laudario/masks/models.py
@@ -1,55 +1,44 @@
 from django.db import models
 
-
-
-class Exam(models.Model):
-    long_description = models.CharField(max_length=50)
-    short_description = models.CharField(max_length=25)
+class Exame(models.Model):
+    descricao = models.CharField(max_length=50)
     initials = models.CharField(max_length=3)
-    def __str__(self):
-        return self.long_description
-    
-class Specialty(models.Model):
-    long_description = models.CharField(max_length=50)
-    short_description = models.CharField(max_length=25)
-    def __str__(self):
-        return self.long_description
-
-class System(models.Model):
-    long_description = models.CharField(max_length=50)
-    short_description = models.CharField(max_length=25)
-    specialty = models.ForeignKey(Specialty, on_delete=models.CASCADE)
-    def __str__(self):
-        return self.long_description
-
-
-
-    #Available Organs (it has to be updated when a new organ is added. 
-class Organ(models.Model):
-    long_description = models.CharField(max_length=50)
-    short_description = models.CharField(max_length=25)
-    string_variable = models.CharField(max_length=25)
-    system = models.ForeignKey(System, on_delete=models.CASCADE)
-    def __str__(self):
-        return self.long_description
-
-#Normal masks (templates)
-class Mask(models.Model):
-    system = models.ForeignKey(System, on_delete=models.CASCADE)
-    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
-    contrast = False
-    mask_name = models.CharField(max_length=50)
-    title = models.CharField(max_length=50)
-    technique_header = models.CharField(max_length=50)
-    technique = models.CharField(max_length=500)
-    report_header = models.CharField(max_length=500)
-
-    #The available Organs, when added to the database, have to be update here.
-    figado = models.TextField(default='')
-
-    
 
     def __str__(self):
-        return self.mask_name
+        return self.descricao
+
+class Especialidade(models.Model):
+    descricao = models.CharField(max_length=50)
+    def __str__(self):
+        return self.descricao
+
+class Sistema(models.Model):
+    especialidade = models.ForeignKey(Especialidade, on_delete=models.CASCADE)
+    descricao = models.CharField(max_length=50)
+    def __str__(self):
+        return self.descricao
+
+# Normal masks (templates)
+class Mascara(models.Model):
+    sistema = models.ForeignKey(Sistema, on_delete=models.CASCADE)
+    exame = models.ForeignKey(Exame, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=50)
+    titulo = models.CharField(max_length=50)
+    tecnica_header = models.CharField(max_length=50)
+    tecnica = models.CharField(max_length=500)
+    relatorio_header = models.CharField(max_length=500)
+    def __str__(self):
+        return self.nome
+
+
+class TopicoLaudo(models.Model):
+    mascara = models.ForeignKey(Mascara, on_delete=models.CASCADE)
+    orgao = models.CharField(max_length=50)
+    relatorio = models.CharField(max_length=500)
+    def __str__(self):
+        return self.orgao
+
+
+
 
 
