b1e299969211360ed62244ab1a7eeeb404418b3d
Arthur Neto <netofarthur@gmail.com>
2020-07-22 14:44:22 -0300

    Coloca cursor no input quando é variável quantitativa.

--- laudario/masks/static/masks/js/mascara.js
+++ laudario/masks/static/masks/js/mascara.js
@@ -440,6 +440,42 @@ mostrarBotaoPopularSeNecessario();
 
     }
 
+    function selecionarVariavel() {
+       var variaveisDiv = document.getElementById("template_name_variaveis");
+
+        if(variaveisDiv.firstChild != null) {
+            if(variaveisDiv.firstChild.children[1].tagName == "input") {
+                variaveisDiv.firstChild.children[1].focus();
+                variaveisDiv.firstChild.children[1].select();
+            } else {
+                variaveisDiv.firstChild.children[1].focus();
+
+            }
+        }
+
+
+    }
+
+
+    function insertAtCursor(myField, myValue) {
+        //IE support
+        if (document.selection) {
+            myField.focus();
+            sel = document.selection.createRange();
+            sel.text = myValue;
+        }
+        //MOZILLA and others
+        else if (myField.selectionStart || myField.selectionStart == '0') {
+            var startPos = myField.selectionStart;
+            var endPos = myField.selectionEnd;
+            myField.value = myField.value.substring(0, startPos)
+                + myValue
+                + myField.value.substring(endPos, myField.value.length);
+        } else {
+            myField.value += myValue;
+        }
+    }
+
 
     function mostrarProximaVariavel() {
            var variaveisDiv = document.getElementById("template_name_variaveis");
@@ -452,6 +488,7 @@ mostrarBotaoPopularSeNecessario();
                    variaveisDiv.firstChild.style.display = "block"
 
            }
+           selecionarVariavel();
 
     }
 
