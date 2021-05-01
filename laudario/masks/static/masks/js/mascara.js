c0848cdd0fd9c1f71eb883b1f3adedf31e01c70e
Arthur Neto <netofarthur@gmail.com>
2021-05-01 20:47:11 -0300

    Pinta lugar onde está no navbar e aumento do tinymce para editar laudo (aumento vertical).

--- laudario/masks/static/masks/js/mascara.js
+++ laudario/masks/static/masks/js/mascara.js
@@ -2609,6 +2609,7 @@ document.getElementById("titulo").style.margin = ".5rem";
                             browser_spellcheck: true,
                             selector: '#mascara_div',
                                  language: 'pt_BR',
+                                 height: "600",
 
 
 
